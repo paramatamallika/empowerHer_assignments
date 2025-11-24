f3544fc7ef5733c14b532a5ef94e8fa5f7f48deb
paramatamallika <paramatamallika@gamil.com>
2025-11-24 21:50:50 +0530

    eight

---
+++ 24-11-2025/8.js
@@ -0,0 +1,24 @@
+if (true) {
+  let x = 10;
+  var y = 20;
+}
+console.log(y);
+console.log(x);
+const profile = {
+  user: {
+    details: {
+      email: "test@mail.com"
+    }
+  }
+};
+
+console.log(profile.user.details.email);   
+console.log(profile.user.details.phone?.number); 
+const data = {
+  product: {
+    info: null
+  }
+};
+
+console.log(data.product.info.name); 
+

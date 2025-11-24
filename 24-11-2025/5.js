4e0946ce26a387f6fac774e0ead97f2787f0addd
paramatamallika <paramatamallika@gamil.com>
2025-11-24 21:44:58 +0530

    fifth

---
+++ 24-11-2025/5.js
@@ -0,0 +1,14 @@
+const username = "John";
+const course = "JavaScript";
+
+console.log(`Hello ${username}, welcome to the ${course} course!`);
+const name = "Sam";
+const age = 21;
+
+const student = {
+  name,
+  age,
+  greet() {
+    console.log("Hello");
+  }
+};

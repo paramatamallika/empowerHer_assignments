603a0c569e5cc6016e485100a7d60fb8533d0f7a
paramatamallika <paramatamallika@gamil.com>
2025-11-26 12:01:19 +0530

    second

---
+++ Event_loops(timers)/asychronous.js
@@ -0,0 +1,12 @@
+function timer(duration, onComplete) {
+  console.log(`Timer started for ${duration} ms...`);
+
+  setTimeout(() => {
+    const message = `Timer of ${duration} ms finished`;
+    onComplete(message);
+  }, duration);
+}
+
+timer(2000, function(message) {
+  console.log(message);
+});
